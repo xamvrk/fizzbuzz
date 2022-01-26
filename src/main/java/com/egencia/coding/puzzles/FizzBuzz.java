bcc10e874365dd8be4f3fd2cb91a01e7ef94b9a4
Xavier Vrac <xavrac@Xaviers-MacBook-Air.local>
2022-01-26 14:34:37 +0100

    Adding source files

---
+++ src/main/java/com/egencia/coding/puzzles/FizzBuzz.java
@@ -0,0 +1,12 @@
+package com.egencia.coding.puzzles;
+
+/**
+ * This program plays the game "Fizzbuzz".  It counts to 20, replacing each
+ * multiple of 3 with the word "fizz", each multiple of 5 with the word "buzz",
+ * and each multiple of both with the word "fizzbuzz".
+ * otherwise it prints the number
+ */
+public class FizzBuzz {
+
+    // TODO your code here
+}
---
+++ src/test/java/com/egencia/coding/puzzles/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+package com.egencia.coding.puzzles;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+
+}
